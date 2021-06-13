5c68e51961d8b03a9440ffa1edc0787f8cae37c0
mcdavidxii <realmcdavid1@gmail.com>
2021-06-13 19:54:37 +0200

    hero section video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,11 +2,14 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import vid from './video-2.mp4';
 
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src='/videos/video-1.mp4' autoPlay loop muted />
+            <video  autoPlay loop muted>
+                <source src={vid}  type="video/mp4"/>
+            </video>
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
             <div className='hero-btns'>
